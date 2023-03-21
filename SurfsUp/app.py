d659e8b8900ecac833e3550d979b239f4e1ae272
Abeda <abeda@MacBook-Air.local>
2023-03-21 15:03:10 -0400

    updated app.py file

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -20,13 +20,14 @@ engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 # reflect an existing database into a new model
 Base = automap_base()
 # reflect the tables
-Base.prepare(engine, reflect=True)
+Base.prepare(autoload_with=engine)
 
 #save references to each table
 Measurement = Base.classes.measurement
-Station=Base.classes.station
+Station= Base.classes.station
 
 
+session = Session(engine)
 #################################################
 # Flask Setup
 #################################################
@@ -73,34 +74,18 @@ def precipitation():
 
 @app.route("/api/v1.0/stations")
 def stations():
-    session = Session(engine)
-    
-    """Return the list of stations from the dataset"""
-    # query all stations
-    results = session.query(Station.station, Station.name, Station.latitude, Station.longitude, Station.elevation).all()
-    
+    """Return a list of stations."""
+    results = session.query(Station.station).all()
+
     session.close()
-    
-    # Convert list of tuples into normal list
-    stations_list = []
-    for station, name, latitude, longitude, elevation in results:
-        station_dict = {}
-        station_dict['station'] = station
-        station_dict['name'] = name
-        station_dict['latitude'] = latitude
-        station_dict['longitude'] = longitude
-        station_dict['elevation'] = elevation
-        station_list.append(station_dict)
-        
-        station_list.append(station_dict)
-        
-    return jsonify(station_list)
-    
+
+    # Unravel results into a 1D array and convert to a list
+    stations = list(np.ravel(results))
+    return jsonify(stations=stations)
     
 @app.route("/api/v1.0/tobs")
 def tobs():
     
-    session = Session(engine)
     
     #Calculate the date 1 year ago from last date in database
     """Return a list of all temperatures in the past year"""
@@ -120,10 +105,9 @@ def tobs():
     return jsonify(past_12_months)
     
 
-@app.route("/api/v1.0/temp/start")
+@app.route("/api/v1.0/temp/<start>")
 def start():
     
-    sessions= Session(engine)
     
     """Return a list of the minimum, average and maximum temperatures for the start"""
     
@@ -132,7 +116,7 @@ def start():
             func.avg(Measurement.tobs),
             func.max(Measurement.tobs)]
             
-    temp_query = session.query(*temperatures).filter_by(Measurement.date > start).all()
+    temp_query = session.query(*temperatures).filter(Measurement.date >= start).all()
     Start_list =[{"TMIN": Query[0][1]},
                 {"TAVG": Query[0][2]},
                 {"TMAX": Query[0][3]}]
@@ -142,7 +126,6 @@ def start():
 
 @app.route("/api/v1.0/temp/start/end")
 def end():
-    session = Session(engine)
     
     """Return a list of the minimum, average and maximum temperatures for the start-end"""
     temperatures = [Measurement.station,
